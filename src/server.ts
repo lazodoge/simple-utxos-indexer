c9ee78de925c8701a52090bca4fb517104e65792
samirneupane302 <neupanesamir234@gmail.com>
2025-11-13 19:18:03 +0545

    added rate limitting

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 15 minutes
-  limit: 1,
+  limit: 30,
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
